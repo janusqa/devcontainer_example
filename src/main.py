1072f1d7834e045296e31d351d9de9437ed6d8d6
Janus Developer <dev@retrievo.net>
2022-06-18 17:47:25 -0400

    First Push

--- src/main.py
+++ src/main.py
@@ -7,5 +7,5 @@ from payload.example_2 import run_spark_example_2
 if __name__ == "__main__":
     load_dotenv()
     spark = SparkConfig("employees").context
-    run_spark_example_1(spark)
+    run_spark_example_2(spark)
     spark.stop()
--- src/payload/example_2.py
+++ src/payload/example_2.py
@@ -1,5 +1,6 @@
 import os
 
+
 def run_spark_example_2(spark):
-    rdd = spark.sparkContext.textFile("README.md")
+    rdd = spark.sparkContext.textFile("file:///home/vscode/workspace/README.md")
     rdd.count()
