0ffcbbba1d97fc3bf5b96cf5e2e9308255047884
Janus Developer <dev@retrievo.net>
2022-06-18 20:12:58 -0400

    Fix distruted file system

--- src/payload/example_2.py
+++ src/payload/example_2.py
@@ -3,12 +3,5 @@ from pyspark.sql import SparkSession
 
 
 def run_spark_example_2(spark: SparkSession):
-    try:
-        with open("README.md", mode="r") as f:
-            fc = f.readlines()
-    except (FileNotFoundError):
-        print("File not found.")
-        spark.stop()
-    else:
-        rdd = spark.sparkContext.parallelize(fc)
-        print(rdd.count())
+    rdd = spark.sparkContext.textFile("/opt/bitnami/spark/data/README.md")
+    print(rdd.count())
